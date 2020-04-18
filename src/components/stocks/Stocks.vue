ecc890bb91b2e522960b177ce95731400eb68016
moisesmengo <moisesmengo@live.com>
2020-04-18 16:27:55 -0300

    configurando o vuex

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,16 +8,11 @@ import Stock from './Stock'
 
 export default {
     components: {Stock},
-    data() {
-        return {
-            stocks: [
-                {id: 1, name: 'Cabaré de Julieta', price: 140},
-                {id: 2, name: 'Xéba de Zé Adalto', price: 60},
-                {id: 3, name: 'Zona de Zefinha Cangaceira', price: 85},
-                {id: 4, name: 'Puteiro XXX-press', price: 140},
-            ]
+    computed:{
+        stocks(){
+            return this.$store.getters.stocks
         }
-    },
+    }
 }
 </script>
 <style lang="css">
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    {id: 1, name: 'Cabaré de Julieta', price: 140},
+    {id: 2, name: 'Xéba de Zé Adalto', price: 60},
+    {id: 3, name: 'Zona de Zefinha', price: 85},
+    {id: 4, name: 'Puteiro XXX-press', price: 140},
+]
