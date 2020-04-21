c259218bd5d6c9ed2b6b1e76250a80c18f44c979
moisesmengo <moisesmengo@live.com>
2020-04-20 21:48:39 -0300

    adicionando saldo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
-        <h1>Home</h1>
+        <h1 class="display-3 font-weight-light mb-4">Negocie e Consulte suas Ações</h1>
+        <v-sheet :elevation="8" class="pa-2" color="#7986CB">
+            <v-icon class="white--text mr-3">info</v-icon>
+            <span class="headline white--text font-weight-light">Você pode Salvar & Carregar os Dados</span>
+        </v-sheet>
+
+        <v-sheet :elevation="8" class="pa-2 mt-3" color="#66BB6A" >
+            <v-icon class="white--text mr-3">info</v-icon>
+            <span class="headline white--text font-weight-light">
+                Clique em Finalizar Dia para iniciar um novo dia!
+            </span>
+        </v-sheet>
+        <v-divider class="my-4"></v-divider>
+        <p class="display-1"><strong>Seu Saldo:</strong> {{funds}}</p>
     </div>
 </template>
 <script>
 export default {
-    
+    computed:{
+        funds(){
+            return this.$store.getters.funds
+        }
+    }
 }
 </script>
 <style lang="css">
