fc5f8722dc4c2275f97eeff2f2c4073949ffe2c5
moisesmengo <moisesmengo@live.com>
2020-04-21 22:32:22 -0300

    validaçoes

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -9,10 +9,12 @@
         </v-card>
         <v-card>
             <v-container fill-height>
-                <v-text-field label="Quantidade" type="number" v-model.number="quantity"/>
+                <v-text-field label="Quantidade" type="number" v-model.number="quantity"
+                    :error="insufficientQuantity || !Number.isInteger(quantity)"
+                />
                 <v-btn class="blue darken-3 white--text" 
-                    :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-                    @click="sellStock">Vender</v-btn>
+                    :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)"
+                    @click="sellStock">{{ insufficientQuantity ? 'Insuficiente' : 'Vender'}}</v-btn>
             </v-container>
         </v-card>
     </v-flex>
@@ -24,6 +26,11 @@ export default {
     data() {
         return {
             quantity: 0,
+        }
+    },
+    computed:{
+        insufficientQuantity(){
+            return this.quantity > this.stock.quantity
         }
     },
     methods: {
